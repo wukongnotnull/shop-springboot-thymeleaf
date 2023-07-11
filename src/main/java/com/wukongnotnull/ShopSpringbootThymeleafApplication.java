4981881bf801acee38cea0aa00eeb61fcba89c46
wukong <wukong@wukong.lan>
2023-07-11 10:38:58 +0800

    git init

---
+++ src/main/java/com/wukongnotnull/ShopSpringbootThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.wukongnotnull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSpringbootThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopSpringbootThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wukongnotnull/ShopSpringbootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wukongnotnull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSpringbootThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
